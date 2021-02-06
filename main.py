ce2fb9f67b1df0f17d3de02c20462980a161099d
weia88 <weiandrew88@gmail.com>
2021-02-06 15:12:51 -0800

    Minor restructuring

--- main.py
+++ main.py
@@ -9,17 +9,12 @@ food_cal = {}
 
 
 #Code review on param --> Self and master usage
-class Main1:
+class StartPage:
     def __init__(self, master):
         self.master = master
+        self.master.title('Food Journal Home Page')
         self.main_frame = tk.Frame(self.master)
-        self.master.title('Food Journal')
-        self.main_frame.pack(ipadx=50, ipady=50)
-
-        # Button that when pressed creates and opens a new class/window 
-        # self.button1 = tk.Button(self.main_frame, text = 'New Window', width = 25, command = self.display_data)
-        # self.button1.pack()
-
+        self.main_frame.pack(ipadx=50, ipady=50)       
 
         label_date = tk.Label(self.main_frame, text="Date :")
         label_date.grid(row=0, column=0)
@@ -41,15 +36,21 @@ class Main1:
         self.entry_calories = tk.Entry(master=self.main_frame)
         self.entry_calories.grid(row=3, column=1)
 
-        buttonFrame = tk.Frame()
-        buttonFrame.pack()
+        self.button_frames = tk.Frame()
+        self.button_frames.pack()
 
-        self.btn_submit = tk.Button(buttonFrame, text="Submit", command=self.submit_entry) #in case its needed (command=lambda : self.submit_entry(entry_date.get(), entry_time.get(), entry_food.get(), entry_calories.get())
+        #Button that when pressed creates and opens a new class/window 
+        self.btn_display = tk.Button(self.button_frames, text = 'New Window', width = 20, bg = 'lightblue', command = self.display_data)
+        self.btn_display.pack()
+
+        self.btn_submit = tk.Button(self.button_frames, text="Submit", width = 10, command = self.submit_entry) #in case its needed (command=lambda : self.submit_entry(entry_date.get(), entry_time.get(), entry_food.get(), entry_calories.get())
         self.btn_submit.pack(side=tk.RIGHT)
 
-        self.btn_clear = tk.Button(master=buttonFrame, text="Clear", command=self.clear_entry)
+        self.btn_clear = tk.Button(self.button_frames, text="Clear", width = 10, command = self.clear_entry)
         self.btn_clear.pack(side=tk.RIGHT)
 
+        self.btn_quit = tk.Button(self.button_frames, text="Quit", width = 10, command = self.close_windows)
+        self.btn_quit.pack(side=tk.RIGHT)
 
 
     def clear_entry(self):
@@ -58,7 +59,7 @@ class Main1:
         self.entry_food.delete(0, tk.END)
         self.entry_calories.delete(0, tk.END)
 
-
+    # Non Operational right now
     def submit_entry(self): #coressponding to comment about lamda --> add params date, time, food, calories
         jm.initialize_data()
         temp = jm.add_food(self.entry_date.get(), self.entry_time.get(), self.entry_food.get(), self.entry_calories.get())
@@ -66,24 +67,38 @@ class Main1:
         jm.write_json('food_journal', food_journal)
         self.clear_entry()
 
-    # def display_data(self):
-    #         self.newWindow = tk.Toplevel(self.master)
-    #         self.app = Demo2(self.newWindow)
-
-class Demo2:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.quitButton.pack()
-        self.frame.pack()
+    def display_data(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = JournalWindow(self.newWindow)
 
     def close_windows(self):
         self.master.destroy()
+
+class JournalWindow:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Displaying Journal')
+        self.frame = tk.Frame(self.master)
+
+        self.btn_show = tk.Button(self.frame, text = 'Show', width = 25, command = self.show_data)
+        self.btn_show.pack()
+        
+        self.btn_quit = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.btn_quit.pack()
+        
+        self.frame.pack()
+
+    def show_data():
+        #Something with Json
+        return()
+    
+    def close_windows(self):
+        self.master.destroy()
+    
         
 def main():
     root = tk.Tk()
-    app = Main1(root)
+    app = StartPage(root)
     root.mainloop()
 
 if __name__ == '__main__':
