4b7ba8bf0b2e2e86cde9fdbddad7daf65bc399a2
weia88 <weiandrew88@gmail.com>
2021-02-11 21:26:41 -0800

    Code Clarity

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import JSONmaker as jm
 import tkinter as tk
 from tkinter import messagebox
-import datetime
+from datetime import datetime
 
 
 # Code review on param --> Self and master usage
@@ -18,7 +18,7 @@ class StartPage:
         label_date = tk.Label(self.main_frame, text="Date :")
         label_date.grid(row=0, column=0)
         self.entry_date = tk.Entry(self.main_frame)
-        self.entry_date.insert(0, datetime.datetime.today().strftime("%m/%d/%Y"))
+        self.entry_date.insert(0, datetime.today().strftime("%m/%d/%Y"))
         self.entry_date.grid(row=0, column=1)
         label_date_format = tk.Label(self.main_frame, text="MM/DD/YYYY")
         label_date_format.grid(row=0, column=2)
@@ -26,7 +26,7 @@ class StartPage:
         label_time = tk.Label(master=self.main_frame, text="Time :")
         label_time.grid(row=1, column=0)
         self.entry_time = tk.Entry(master=self.main_frame)
-        self.entry_time.insert(0, datetime.datetime.now().time().strftime("%H:%M"))
+        self.entry_time.insert(0, datetime.now().time().strftime("%H:%M"))
         self.entry_time.grid(row=1, column=1)
         label_time_format = tk.Label(self.main_frame, text="24 Hours")
         label_time_format.grid(row=1, column=2)
@@ -73,8 +73,8 @@ class StartPage:
             jm.add_food(self.entry_date.get(), self.entry_time.get(), self.entry_food.get(),
                         self.entry_calories.get())
             self.clear_entry()
-            self.entry_date.insert(0, datetime.datetime.today().strftime("%m/%d/%Y"))
-            self.entry_time.insert(0, datetime.datetime.now().time().strftime("%H:%M"))
+            self.entry_date.insert(0, datetime.today().strftime("%m/%d/%Y"))
+            self.entry_time.insert(0, datetime.now().time().strftime("%H:%M"))
 
     def validate(self):
         error_found = [False, ""]
@@ -84,11 +84,11 @@ class StartPage:
         calorie_valid = [True, "Calorie needs to be integer"]
         error_list = [date_valid, time_valid, food_valid, calorie_valid]
         try:
-            datetime.datetime.strptime(self.entry_date.get(), '%m/%d/%Y')
+            datetime.strptime(self.entry_date.get(), '%m/%d/%Y')
         except ValueError:
             date_valid[0] = False
         try:
-            datetime.datetime.strptime(self.entry_time.get(), '%H:%M')
+            datetime.strptime(self.entry_time.get(), '%H:%M')
         except ValueError:
             time_valid[0] = False
         if self.entry_food.get() == "":
@@ -116,15 +116,16 @@ class JournalWindow:
     def __init__(self, master):
         self.master = master
         self.master.title('Displaying Journal')
-        self.frame = tk.Frame(self.master)
-        self.frame.pack(side=tk.TOP)
+        self.frame = tk.Frame(self.master, width=500, height=800)
+        self.frame.pack()
+        self.frame.pack_propagate(0)
 
         journal_data = jm.read_json("food_journal")  # Example being the json file name
 
-        self.btn_show = tk.Button(self.frame, text='Show', width=25, command=lambda: self.show_data(journal_data))
+        self.btn_show = tk.Button(self.frame, text='Show All', width=25, command=lambda: self.show_data(journal_data))
         self.btn_show.pack(side=tk.RIGHT)
 
-        self.btn_filter = tk.Button(self.frame, text=' Filter By', width=25,
+        self.btn_filter = tk.Button(self.frame, text='Filter By', width=25,
                                     command=lambda: self.filter_by_date(journal_data))
         self.btn_filter.pack(side=tk.RIGHT)
 
@@ -139,8 +140,8 @@ class JournalWindow:
         self.entry_filter = tk.Entry(self.filter_frame)
         self.entry_filter.pack(side=tk.BOTTOM)
 
-    # Displays entire Journal Entry content
     def show_data(self, journal_data):
+    # Displays entire Journal Entry content        
         self.frame3 = tk.Frame(self.master)
         self.frame3.pack()
 
@@ -159,39 +160,22 @@ class JournalWindow:
         self.txt_box.configure(state="disable")
         self.txt_box.pack()
 
-    # Filters Journal Entry content before showing a specific
-    # Going to need to call show_data within this function in the future, where filter runs before show
-    # TODO: Add mutiple date and month options
+
     def filter_by_date(self, journal_data):
-        data = journal_data
+        # TODO: Add mutiple date and month options
+
         input_filter = self.entry_filter.get()
-
-        # if input is date {
-        #   Execute belows code
-        # } 
-        # Waiting on check date method
-
-        self.frame3 = tk.Frame(self.master)
-        self.frame3.pack()
-
-        self.txt_box = tk.Text(self.frame3)
-
-        for key, value in journal_data.items():
-            if input_filter in key:
-                self.txt_box.insert(tk.END, key + "\n", ("key_date", 0, tk.END))
-                self.txt_box.tag_configure("key_date", foreground="red", font=" TkFixedFont")
-                # Perhaps use Enumerate?
-                for element in value:
-                    # Change Name, Time, and Calories color/font/size
-                    self.txt_box.insert(tk.END, "Name: " + element["name"] + "\n", ("next_line", 0, tk.END))
-                    self.txt_box.insert(tk.END, "Time: " + element["time"] + "\n", ("next_line", 0, tk.END))
-                    self.txt_box.insert(tk.END, "Calories: " + str(element["calories"]) + "\n",
-                                        ("next_line", 0, tk.END))
-            else:
-                print(key + "sad")
-
-        self.txt_box.configure(state="disable")
-        self.txt_box.pack()
+        if not input_filter: # Checks empty string
+            try: # Check if Date format
+                datetime.strptime(input_filter, '%m/%d/%Y')
+            except ValueError:
+                messagebox.showerror("Invalid Date Input", "Invalid Date Input")
+        else: 
+            filtered_data = {}
+            for key, value in journal_data.items():
+                if input_filter in key:
+                    filtered_data[key] = value
+            self.show_data(filtered_data)
 
     def close_windows(self):
         self.master.destroy()
